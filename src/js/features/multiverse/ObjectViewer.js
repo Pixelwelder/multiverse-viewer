6bb0480b08d7c92a27863df3436bee7ad007c843
Zack Jordan <pixelwelders@gmail.com>
2020-08-15 16:17:47 -0500

    Get stuff working again.

--- src/js/features/multiverse/ObjectViewer.js
+++ src/js/features/multiverse/ObjectViewer.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Button } from 'react-bootstrap';
-import ReactMarkdown from "react-markdown";
 
 import { ROOM } from '../../multiverse/constants';
 
--- src/js/features/multiverse/Story.js
+++ src/js/features/multiverse/Story.js
@@ -1,7 +1,5 @@
 import React from 'react';
-import { actions as multiverseActions, selectors as multiverseSelectors } from "./multiverseSlice2";
-import ObjectViewer from "./ObjectViewer";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import { selectors } from "./selectors";
 import ReactMarkdown from "react-markdown";
 
@@ -10,7 +8,7 @@ const Story = () => {
 
   return (
     <div className="story">
-      {/*<ReactMarkdown source={selectedObject.description || ''} />*/}
+      <ReactMarkdown source={selectedObject?.description || ''} />
     </div>
   );
 };
